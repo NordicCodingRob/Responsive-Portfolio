2bd50d2f4085d806a79f7929accfca82e3ee39e5
Sam Llamas <sam.llamas@utexas.edu>
2018-06-05 15:01:41 -0500

    fixed handlebars, yeeeee

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
+var path = require("path")
 
 // Create an instance of the express app.
 var app = express();
@@ -9,6 +10,7 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
