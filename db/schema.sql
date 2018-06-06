5c1b1a2397da90eb77be84a021418f6ace35d5e9
Sam Llamas <sam.llamas@utexas.edu>
2018-06-06 14:09:10 -0500

    folder fix

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,3 @@
--- Drops the blogger if it exists currently --
 DROP DATABASE IF EXISTS games_site;
--- Creates the "blogger" database --
+
 CREATE DATABASE games_site;
---
+++ models/scores.js
@@ -0,0 +1,9 @@
+module.exports = function(sequelize, DataTypes) {
+    var Scores = sequelize.define("Scores", {
+      gameName: DataTypes.STRING,
+      scoreHolder: DataTypes.STRING,
+      highScore: DataTypes.INTEGER
+    });
+    return Scores;
+};
+  
